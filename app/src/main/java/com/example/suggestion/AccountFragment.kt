495d2b18b6240e46993c764c9def4d6ac148bcd7
Amelie <marie-amelie.heraud@never2wait.com>
2024-11-20 19:04:36 +0100

    Refactorisation de l'infrastructure

---
+++ app/src/main/java/com/example/suggestion/AccountFragment.kt
@@ -0,0 +1,45 @@
+package com.example.suggestion
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.suggestion.SelectDateHelper.showDatePicker
+
+class AccountFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Redirection vers ResetPasswordActivity lors du clic sur le texte
+        val resetPassword = view.findViewById<TextView>(R.id.resetpassword)
+        resetPassword.setOnClickListener {
+            val intent = Intent(requireActivity(), ResetPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        // Référence à l'EditText pour la date de naissance
+        val dateOfBirthEditText = view.findViewById<EditText>(R.id.dateOfBirth)
+
+        // Ouvrir le sélecteur de date lors du clic sur l'EditText
+        dateOfBirthEditText.setOnClickListener {
+            showDatePicker(requireContext()) { selectedDate ->
+                // Afficher la date sélectionnée dans l'EditText
+                dateOfBirthEditText.setText(selectedDate)
+                Toast.makeText(requireContext(), "Anniversaire sélectionné : $selectedDate", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/suggestion/AddCategorieDialogFragment.kt
@@ -0,0 +1,82 @@
+package com.example.suggestion
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.Toast
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.DialogFragment
+import com.example.suggestion.SelectDateHelper.showDatePicker
+
+class AddCategorieDialogFragment : DialogFragment(), AdapterView.OnItemSelectedListener {
+
+    private lateinit var customSpinner: Spinner
+    private lateinit var expenseDetail: EditText
+    private lateinit var priceCost: EditText
+    private lateinit var monthDepenseEditText: EditText
+    private lateinit var submitButton: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.activity_add_categorie, container, false)
+
+        customSpinner = view.findViewById(R.id.customSpinner)
+        customSpinner.onItemSelectedListener = this
+        expenseDetail = view.findViewById(R.id.expenseDetail)
+        priceCost = view.findViewById(R.id.priceCost)
+        monthDepenseEditText = view.findViewById(R.id.monthDepense)
+        submitButton = view.findViewById(R.id.buttonSubmit)
+
+        val customList = arrayListOf(
+            CategorieListItems("Dépenses quotidiennes", R.drawable.ic_shopping),
+            CategorieListItems("Maison", R.drawable.ic_home),
+            CategorieListItems("Loisir", R.drawable.ic_loisir)
+        )
+
+        val adapter = CustomAdapter(requireContext(), customList)
+        customSpinner.adapter = adapter
+
+        // Ouvrir le sélecteur de date lors du clic sur l'EditText
+        monthDepenseEditText.setOnClickListener {
+            showDatePicker(requireContext()) { selectedDate ->
+                monthDepenseEditText.setText(selectedDate)
+                Toast.makeText(context, "Sélectionné la date de votre dépense : $selectedDate", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        submitButton.setOnClickListener {
+            val expenseDetailText = expenseDetail.text.toString()
+            val priceCostText = priceCost.text.toString()
+            val monthDepenseText = monthDepenseEditText.text.toString()
+
+            if (expenseDetailText.isNotEmpty() && priceCostText.isNotEmpty() && monthDepenseText.isNotEmpty()) {
+                dismiss() // Ferme la modale
+                Toast.makeText(context, "Dépense ajoutée avec succès", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Veuillez saisir tous les champs", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return view
+    }
+
+    override fun onNothingSelected(adapterView: AdapterView<*>?) {}
+
+    override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val item = adapterView?.getItemAtPosition(position) as CategorieListItems
+        Toast.makeText(context, item.spinnerText, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            setTitle("Ajouter une catégorie")
+        }
+    }
+}
